59ffe8995bba16b9f73e5b76994faff584a3157b
Seth Hyslop <sahyslop@umich.edu>
2026-02-06 12:42:23 -0500

    resume tweak and blogpost

--- app/ui/Footer.tsx
+++ app/ui/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
     <footer className="fixed bottom-0 left-0 right-0 p-6 bg-background">
       <nav className="max-w-3xl mx-auto flex justify-center gap-8 text-sm font-mono">
         <TextScrambleLink href="/" text="Home" />
-        {/* <TextScrambleLink href="/blog" text="Blog" /> */}
+        <TextScrambleLink href="/blog" text="Blog" />
         <TextScrambleLink href="/resume.pdf" text="Resume" external />
       </nav>
     </footer>
